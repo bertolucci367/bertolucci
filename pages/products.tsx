873d90f9fcc21b130848bb1e73cfed2c77fb38f6
Rodrigo Pestana <rodrigo.pest@gmail.com>
2020-12-27 00:44:17 -0300

    upgrade package and apply hover effects

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,12 +1,57 @@
+import xw from 'xwind'
+import styled from '@emotion/styled'
 import Layout from '../components/Layout'
 
+const Hover = styled.div([
+  xw`opacity-0`,
+  {
+    transition: 'opacity 350ms ease',
+  },
+])
+
+const Name = styled.h2(xw`text-14px`)
+
+const Design = styled.p(xw`text-12px`)
+
+const Card = styled.li({
+  [`:hover ${Hover}`]: {
+    opacity: 1,
+  },
+  height: '270px',
+})
+
 const Products = () => {
+  const products = []
+
+  for (let i = 0, l = 83; i < l; i++) {
+    products.push({ id: i, name: `Product ${i}`, design: `oficina bertolucci` })
+  }
+
   return (
-    <>
-      <Layout>
-        <h1>Products</h1>
-      </Layout>
-    </>
+    <Layout>
+      <h1 css={xw`h-0`}>Produtos</h1>
+      <ul
+        css={[
+          `grid-template-columns: repeat(auto-fill, minmax(225px, 1fr))`,
+          xw`grid gap-x-1`,
+        ]}
+      >
+        {products.map((product) => (
+          <Card key={product.id}>
+            <div
+              css={[
+                `width: 100%; height: 200px; background: url('${`https://placedog.net/360/280`}')`,
+                xw`bg-gray-200 bg-center bg-cover`,
+              ]}
+            ></div>
+            <Hover>
+              <Name>{product.name}</Name>
+              <Design>{product.design}</Design>
+            </Hover>
+          </Card>
+        ))}
+      </ul>
+    </Layout>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,19 @@ module.exports = {
   darkMode: 'class',
   theme: {
     fontSize: {
+      '12px': '1.2rem',
       '13px': '1.3rem',
+      '14px': '1.4rem',
       '18px': '1.8rem',
     },
     extend: {
       colors,
     },
   },
-  variants: {},
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    opacity: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus'],
+  },
   plugins: [],
   // xwind options
   xwind: {
