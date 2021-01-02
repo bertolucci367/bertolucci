a2e6875ba5a1833b2fc9ee2d159782f5b2bd4ed1
Rodrigo Pestana <rodrigo.pest@gmail.com>
2021-01-02 13:28:49 -0300

    added all items in the submenu of the products

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,7 +1,6 @@
 import xw from 'xwind'
 import styled from '@emotion/styled'
 import Layout from '../components/Layout'
-import Portal from '../components/Portal'
 
 const Hover = styled.div([
   xw`lg:opacity-0`,
@@ -10,9 +9,9 @@ const Hover = styled.div([
   },
 ])
 
-const Name = styled.h2(xw`text-14px`)
+const NameStyled = styled.h2(xw`text-14px`)
 
-const Design = styled.p(xw`text-12px`)
+const DesignStyled = styled.p(xw`text-12px`)
 
 const Card = styled.li({
   [`:hover ${Hover}`]: {
@@ -22,59 +21,167 @@ const Card = styled.li({
 })
 
 const SubMenu = styled.ul([
-  xw`lg:opacity-0 w-full bg-red-100 absolute left-0 flex lg:justify-center py-4`,
+  xw`lg:opacity-0 lg:hidden w-full bg-red-100 absolute left-0 flex lg:justify-center py-4`,
   {
     transition: 'opacity 350ms ease',
   },
 ])
 
-const MenuItem = styled.li({
+const SubMenuLabel = styled.span(xw``)
+
+const MenuItemStyled = styled.li(xw`mx-4`, {
   [`:hover ${SubMenu}`]: {
     opacity: 1,
     cursor: 'pointer',
     zIndex: 50,
+    display: 'flex',
+  },
+  [`:hover ${SubMenuLabel}`]: {
+    color: 'red',
+    cursor: 'pointer',
   },
 })
 
-const typologies = ['abajur', 'arandela', 'coluna', 'pendente', 'plafom']
-const materials = [
-  'acrílico',
-  'cerâmica',
-  'cortiça',
-  'fibras naturais',
-  'madeira',
-  'metal',
-  'tecido',
-  'vidro',
+const typologies = [
+  { name: 'abajur' },
+  { name: 'arandela' },
+  { name: 'coluna' },
+  { name: 'pendente' },
+  { name: 'plafom' },
 ]
+const materials = [
+  { name: 'acrílico' },
+  { name: 'cerâmica' },
+  { name: 'cortiça' },
+  { name: 'fibras naturais' },
+  { name: 'madeira' },
+  { name: 'metal' },
+  { name: 'tecido' },
+  { name: 'vidro' },
+]
+const designers = [
+  {
+    img:
+      'http://assets2.bertolucci.com.br/uploads/designer_images/image/517ed6eb43493b7e4600009d/thumb_ana-strumpf.jpg',
+    name: 'ana strumpf',
+    slug: 'ana-strumpf',
+  },
+  {
+    img:
+      'http://assets2.bertolucci.com.br/uploads/designer_images/image/56df1be8a7ab750602000003/thumb_atelier-bam.jpg',
+    name: 'atelier bam',
+    slug: 'atelier-bam',
+  },
+  {
+    img:
+      'http://assets2.bertolucci.com.br/uploads/designer_images/image/5098140b43493b4870000006/thumb_beto-galvez-norea-de-vitto.jpg',
+    name: 'beto galvez e nórea de vitto',
+    slug: 'beto-galvez-e-norea-de-vitto',
+  },
+  {
+    img:
+      'http://assets2.bertolucci.com.br/uploads/designer_images/image/509d3b1b43493b540a000012/thumb_atelier-marko-brajovic.jpg',
+    name: 'atelier marko brajovic',
+    slug: 'atelier-marko-brajovic',
+  },
+  {
+    img:
+      'http://assets1.bertolucci.com.br/uploads/designer_images/image/5188f96d43493b130c000005/thumb_camila_sarpi.jpg',
+    name: 'camila sarpi',
+    slug: 'camila-sarpi',
+  },
+  {
+    img:
+      'http://assets2.bertolucci.com.br/uploads/designer_images/image/5098142443493b487000000a/thumb_claudia-moreira-salles.jpg',
+    name: 'claudia moreira salles',
+    slug: 'claudia-moreira-salles',
+  },
+  {
+    img:
+      'http://assets1.bertolucci.com.br/uploads/designer_images/image/5098144943493b4876000004/thumb_Deborah-Roig.jpg',
+    name: 'deborah roig',
+    slug: 'deborah-roig',
+  },
+  {
+    img:
+      'http://assets1.bertolucci.com.br/uploads/designer_images/image/5098143643493b487000000c/thumb_debora-aguiar.jpg',
+    name: 'débora aguiar',
+    slug: 'debora-aguiar',
+  },
+  {
+    img:
+      'http://assets1.bertolucci.com.br/uploads/designer_images/image/517ed62943493b7e4600009c/thumb_etel-carmona.jpg',
+    name: 'etel carmona',
+    slug: 'etel-carmona',
+  },
+  {
+    img:
+      'http://assets2.bertolucci.com.br/uploads/designer_images/image/56deec08a7ab7506ba000001/thumb_fernanda-yamamoto.jpg',
+    name: 'fernanda yamamoto',
+    slug: 'fernanda-yamamoto',
+  },
+  {
+    img:
+      'http://assets1.bertolucci.com.br/uploads/designer_images/image/5098145c43493b4873000008/thumb_fernando-piva.jpg',
+    name: 'fernando piva',
+    slug: 'fernando-piva',
+  },
+]
+
+const families = [
+  '2050',
+  '3D de luxo',
+  '85g',
+  'abaeté',
+  'alma',
+  'apoena',
+  'araucária',
+  'atman',
+  'batuque',
+  'bella',
+  'berimbau',
+  'berloque',
+  'beto galvez e nórea de vitto',
+  'bionda',
+  'canoa',
+]
+
+interface MenuItemProps {
+  name?: string
+  subItems?: any
+  children?: React.ReactNode
+}
+
+const MenuItem = ({ name, subItems, children }: MenuItemProps) => {
+  return (
+    <MenuItemStyled>
+      <SubMenuLabel>{name}</SubMenuLabel>
+      {subItems && (
+        <SubMenu>
+          {subItems.map(({ name, img }) => (
+            <li key={name} css={xw`px-4 py-2 w-1/5`}>
+              {img && <img src={img} />}
+              {name}
+            </li>
+          ))}
+        </SubMenu>
+      )}
+      {children}
+    </MenuItemStyled>
+  )
+}
 
 const subMenu = (
   <nav css={xw`w-full`}>
-    <ul css={xw`flex justify-evenly`}>
-      <MenuItem>
-        tipologia+
-        <SubMenu>
-          {typologies.map((t) => (
-            <li key={t} css={xw`px-4 py-2 w-1/5`}>
-              {t}
-            </li>
-          ))}
-        </SubMenu>
+    <ul css={xw`flex justify-center`}>
+      <MenuItem name="tipologia+" subItems={typologies} />
+      <MenuItem name="materiais+" subItems={materials} />
+      <MenuItem name="designers+" subItems={designers} />
+      <MenuItem name="linhas+" subItems={families} />
+      <MenuItem name="busca">
+        <input type="search" css={xw`border border-gray-500 ml-2`} />
       </MenuItem>
-      <MenuItem>
-        materiais+
-        <SubMenu>
-          {materials.map((v) => (
-            <li key={v} css={xw`px-4 py-2`}>
-              {v}
-            </li>
-          ))}
-        </SubMenu>
-      </MenuItem>
-      <li>designers+</li>
-      <li>linhas+</li>
-      <li>busca</li>
-      <li>baixar catalago</li>
+      <MenuItem name="baixar o catálogo" />
     </ul>
   </nav>
 )
@@ -104,8 +211,8 @@ const Products = () => {
               ]}
             ></div>
             <Hover>
-              <Name>{product.name}</Name>
-              <Design>{product.design}</Design>
+              <NameStyled>{product.name}</NameStyled>
+              <DesignStyled>{product.design}</DesignStyled>
             </Hover>
           </Card>
         ))}
