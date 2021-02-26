cae524e4cc521868f94e16bee72d0d69a3ad3883
Rodrigo Pestana <rodrigo.pest@gmail.com>
2021-02-26 15:25:58 -0300

    changed search to use graphcms

--- pages/produtos/busca/[q].tsx
+++ pages/produtos/busca/[q].tsx
@@ -1,10 +1,22 @@
 import LayoutProduct from '~/components/LayoutProduct'
 import List from '~/components/products/List'
-import { useRouter } from 'next/router'
 import xw from 'xwind'
+import { useEffect } from 'react'
+import { useAppContext } from '~/components/context/AppContext'
+import { useRouter } from 'next/router'
+import { GraphQLClient } from 'graphql-request'
 
 const Search = ({ products }) => {
   const router = useRouter()
+  const shared = useAppContext()
+
+  useEffect(() => {
+    shared.addData({
+      goToLines: false,
+      productClosePath: `/produtos/busca/${router.query.q}`,
+    })
+  }, [])
+
   return (
     <LayoutProduct search={router.query.q as string}>
       {products.length === 0 && (
@@ -18,18 +30,37 @@ const Search = ({ products }) => {
 }
 
 export async function getServerSideProps({ params }) {
-  const host = 'http://bertolucci.com.br/api/produtos/search.json'
-  const res = await fetch(`${host}?q=${params.q}`)
-  const data = await res.json()
-
-  if (!data) {
-    return {
-      notFound: true,
+  const gcms = new GraphQLClient(process.env.GRAPHCMS_API)
+  const { values } = await gcms.request(
+    `
+  query Search($id: String!) {
+    values: products(where: {name_contains: $id}) {
+      id
+      name
+      code
+      slug
+      designer {
+        name
+      }
+      photo {
+        handle
+        height
+        width
+        alt
+      }
+      lines {
+        id
+        slug
+        name
+      }
     }
   }
+  `,
+    { id: params.q },
+  )
 
   return {
-    props: data, // will be passed to the page component as props
+    props: { products: values }, // will be passed to the page component as props
   }
 }
 
