8eaa50496d9b7c50a1fbd9e8587e2c2b759cf22d
Rodrigo Pestana <rodrigo.pest@gmail.com>
2021-08-22 17:01:16 -0300

    added info about tire suas duvidas

--- pages/tire-suas-duvidas.tsx
+++ pages/tire-suas-duvidas.tsx
@@ -2,9 +2,201 @@ import Layout from '~/components/Layout'
 
 function FAQ() {
   return (
-    <Layout title="Política de Privacidade">
-      <div className="grid-in-main mb-20">
-        <h1>Tire suas dúvidas</h1>
+    <Layout title="Tire suas dúvidas">
+      <div className="grid-in-main mb-20 ckeditor">
+        <h1 className="sr-only">Tire suas dúvidas</h1>
+        <h2>A BERTOLUCCI</h2>
+        <p>
+          Fundada em São Paulo no bairro da Lapa há mais de 65 anos, a
+          Bertolucci é um verdadeiro laboratório de criação e valorização do
+          trabalho artístico dos artesões. São pessoas apaixonadas pelo design e
+          com absoluto domínio da técnica artesanal e que instauram, nos
+          detalhes, a individualidade do produto.
+        </p>
+        <p>
+          Com o objetivo de melhorar a sua experiência com os produtos
+          Bertolucci, aqui vão algumas instruções de uso, manutenção e de
+          garantia.
+        </p>
+        <p>Seguem algumas recomendações importantes para a sua luminária.</p>
+
+        <h2>INSTALAÇÃO</h2>
+        <ul>
+          <li>
+            _é recomendável que a instalação seja feita por um profissional
+          </li>
+          <li>_desligar a rede elétrica</li>
+          <li>_verificar se a rede elétrica é 110V ou 220V</li>
+          <li>
+            _utilizar a lâmpada especificada na etiqueta do produto – potência
+            máxima e voltagem
+          </li>
+        </ul>
+
+        <h2>MANUTENÇÃO E LIMPEZA</h2>
+        <ul>
+          <li>
+            TODOS OS PRODUTOS
+            <ul>
+              <li>_ utilizar apenas flanela limpa, seca e macia</li>
+              <li>
+                _ NUNCA utilizar produtos abrasivos, ásperos, à base de
+                solvente, amoníaco ou álcool
+              </li>
+            </ul>
+          </li>
+          <li>
+            CROMADO, POLIDO, ESCOVADO
+            <ul>
+              <li>_ utilizar cera automotiva periodicamente</li>
+              <li>
+                _ para regiões de maior umidade, recomendamos a cada 6 meses, a
+                manutenção das peças com sprays hidrofóbicos como o WD40
+              </li>
+              <li>
+                Obs. Em ambientes expostos à maresia e umidade excessiva, o uso
+                da cera automotiva deve ser efetuado com maior frequência.
+              </li>
+            </ul>
+            <li>
+              MADEIRA
+              <ul>
+                <li>
+                  _ pode ser utilizado pano macio levemente umedecido em água
+                </li>
+              </ul>
+            </li>
+            <li>
+              PINTURA NO METAL
+              <ul>
+                <li>_ pano macio ligeiramente umedecido</li>
+              </ul>
+            </li>
+          </li>
+          <li>
+            TECIDOS EM GERAL, CETIM, CORDA, JUNCO, RAMI, SISAL E TABOA
+            <ul>
+              <li>
+                _ recomenda-se aspirar o pó delicadamente e utilizar escova
+                macia
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <h2>TERMO DE GARANTIA</h2>
+        <p>
+          A BERTOLUCCI assegura garantia contra defeitos de material e de
+          manufatura nos seus produtos pelo período de 01 ano, incluído o prazo
+          legal de garantia de 90 dias. A garantia de acabamentos, partes e
+          peças sujeitas a desgaste natural segue estritamente o prazo legal de
+          90 dias. A BERTOLUCCI limita-se à realização de serviços de reposição
+          de partes e componentes nos termos discriminados a seguir e mediante
+          análise efetuada pela própria BERTOLUCCI.
+        </p>
+
+        <h2>SITUAÇÕES NÃO COBERTAS POR GARANTIA</h2>
+
+        <ul>
+          <li>
+            Danos causados por transporte, ou montagem/instalação incorretos.
+          </li>
+          <li>
+            Mau uso, esforços indevidos, ou qualquer utilização o que não aquele
+            do proposto para o produto.
+          </li>
+          <li>
+            Problemas causados por montagem em desacordo, ou relacionados a
+            adaptações e/ou alterações realizadas no produto.
+          </li>
+          <li>
+            Problemas relacionados a condições inadequadas do local onde o
+            produto foi instalado/colocado, tais como: umidade excessiva,
+            maresia e problemas elétricos do local.
+          </li>
+          <li>
+            Maus tratos, descuido, limpeza e/ou manutenção em desacordo com as
+            instruções.
+          </li>
+          <li>
+            Danos causados por serviços de limpeza ou conserto contratados.
+          </li>
+          <li>
+            Danos causados por acidentes, quedas, sinistros, ataques de pragas
+            ou agentes da natureza.
+          </li>
+          <li>
+            Desgaste excessivo no acabamento por uso intenso ou exposição a
+            condições adversas e não previstas (intempérie, umidade, maresia,
+            frio e calor intensos).
+          </li>
+          <li>
+            Oxidação ou corrosão devido à falta de limpeza, manutenção com
+            produtos inadequados ou exposição à intempérie, umidade ou maresia.
+          </li>
+        </ul>
+
+        <h2>TERMOS E CONDIÇÕES DE VENDAS</h2>
+        <ol>
+          <li>
+            A descrição do pedido e especificações dos produtos são entendidas
+            como aceites desde o momento do pagamento do sinal.
+          </li>
+          <li>
+            O valor pago pelo pedido não contempla qualquer serviço/produto que
+            não esteja explicitamente discriminado, por exemplo, instalação e
+            manutenção da peça.
+          </li>
+          <li>
+            Os produtos por regra são entregues sem lâmpadas, a não ser quando
+            discriminado no pedido.
+          </li>
+          <li>
+            As lâmpadas são vendidas em separado e não damos garantia sobre
+            elas.
+          </li>
+          <li>
+            Salvo condições excepcionais negociadas entre Bertolucci e cliente,
+            nosso prazo estimado de fabricação é de 45 a 60 dias corridos a
+            contar da comprovação do pagamento do sinal, conforme condições de
+            pagamento indicadas no pedido.
+          </li>
+          <li>
+            Por se tratar de um produto artesanal e personalizados, atrasos e
+            imprevistos na produção podem ocorrer sem aviso prévio. Pequenas
+            variações de medidas e acabamentos podem ocorrer.
+          </li>
+          <li>
+            As condições de pagamento descritas no pedido não podem ser
+            alteradas em caso de atraso na entrega dos produtos.
+          </li>
+          <li>
+            Pedidos em fabricação com débitos em atraso terão, sem aviso prévio,
+            sua produção e/ou entrega suspensas até regularização de qualquer
+            pendência.
+          </li>
+          <li>
+            Produtos enviados via transportadora serão feitos com frete e
+            responsabilidade do comprador (FOB).
+          </li>
+          <li>
+            A Bertolucci assegura que todos os seus produtos são testados e
+            embalados em perfeitas condições e que qualquer avaria, vício ou
+            defeito do produto deverá ser denunciada no ato de recebimento.
+          </li>
+          <li>
+            Após o pagamento do sinal, os pedidos não poderão ser cancelados
+            parcial ou totalmente.
+          </li>
+          <li>
+            Nossa garantia é de 1 (um) ano a contar da data de entrega do pedido
+            contra defeitos de fabricação. Qualquer vício de instalação, uso ou
+            depreciação pelo tempo, clima ou qualquer outro vício não será
+            coberto pela nossa garantia e valores adicionais poderão ser
+            cobrados para reparos e manutenção.
+          </li>
+          <li>A Bertolucci não aceita cancelamentos ou devoluções.</li>
+        </ol>
       </div>
     </Layout>
   )
