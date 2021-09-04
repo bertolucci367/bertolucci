34545debebee4e19a92f025a26e8da7d7774c025
Rodrigo Pestana <rodrigo.pest@gmail.com>
2021-09-04 19:07:23 -0300

    added analytics

---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class BertolucciDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head />
+        <Head>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              var _gaq = _gaq || [];
+              _gaq.push(['_setAccount', 'UA-32925919-1']);
+              _gaq.push(['_trackPageview']);
+
+              (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+              })();
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default BertolucciDocument
