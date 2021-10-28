b866d3f3e5c6475c258fb9dd5dd5308c56f4afaa
Rodrigo Pestana <rodrigo.pest@gmail.com>
2021-10-28 13:36:35 -0300

    fixes build

--- pages/cadastro.tsx
+++ pages/cadastro.tsx
@@ -19,7 +19,6 @@ const Signup = () => {
                 phone: '',
                 mail: '',
                 company: '',
-                consultor: '',
                 newsletter: false,
               }}
             />
